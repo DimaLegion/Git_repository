2f05842548f74fe65fe0e1ea662ed581216226df
Dima-VM <legionz5@mail.ru>
2018-06-04 12:30:29 +0300

    1

--- main.c
+++ main.c
@@ -9,17 +9,6 @@ int main(int argc, char **argv)
     fgets(name, 255, stdin);
     printf("length = %d\n", strlen(name)); /* debug line */
     name[strlen(name)-1] = '\0'; /* remove the newline at the end */
-
-<<<<<<< HEAD
     printf("Hello %s!\n", name);
     return 0;
 }
-
-=======
-    printf("Now enter your last name: ");
-    gets(last); /* buffer overflow? what's that? */
-
-    printf("Hello %s %s!\n", first, last);
-    return 0;
-}
->>>>>>> 0184d0bd0c3d42b692666c94f85bb21108595cb3
