2094f0c5e6f8a8c47ea91054972d37f86b6685bc
Dima-VM <legionz5@mail.ru>
2018-06-04 11:43:18 +0300

    Revert "commit +"
    This reverts commit 090e8161b324b9cf166158a3116c577889644fa1.
    
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -3,17 +3,20 @@
 
 int main(int argc, char **argv)
 {
-    char first[255], last[255];
+    char name[255];
 
-    printf("Enter your first name: ");
-    fgets(first, 255, stdin);
-    first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+    printf("Enter your name: ");
+    fgets(name, 255, stdin);
+    printf("length = %d\n", strlen(name)); /* debug line */
 
+<<<<<<< HEAD
     printf("Now enter your last name: ");
     gets(last); /* buffer overflow? what's that? */
 
     printf("Hello, %s %s!\n", first, last);
+=======
+    printf("Hello %s!\n", name);
+>>>>>>> parent of 090e816... commit +
     return 0;
 }
- 
 
